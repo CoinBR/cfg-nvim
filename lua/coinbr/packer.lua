24577b78b03e9f8b9b7bd577dd091463f57d9617
CoinBR <coinbr@gmail.com>
2024-10-14 20:28:57 -0300

    add treesitter

--- lua/coinbr/packer.lua
+++ lua/coinbr/packer.lua
@@ -9,6 +9,8 @@ return require('packer').startup(function(use)
 
 	use { "ellisonleao/gruvbox.nvim" }
 
+	use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+
 	use {
 		'nvim-telescope/telescope.nvim', tag = '0.1.8',
 		-- or                            , branch = '0.1.x',
