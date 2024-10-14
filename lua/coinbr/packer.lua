5078e967d929be6acb9c3b15483c5aa9f9a6f4cc
CoinBR <coinbr@gmail.com>
2024-10-14 20:48:30 -0300

    tidy up packer plugins list

--- lua/coinbr/packer.lua
+++ lua/coinbr/packer.lua
@@ -4,17 +4,8 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-	-- Packer can manage itself
-	use 'wbthomason/packer.nvim'
-
-	use { "ellisonleao/gruvbox.nvim" }
-
-	use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
-
-	use {
-		'nvim-telescope/telescope.nvim', tag = '0.1.8',
-		-- or                            , branch = '0.1.x',
-		requires = { {'nvim-lua/plenary.nvim'} }
-	}
-
+	use 'wbthomason/packer.nvim' -- Packer can manage itself
+	use 'ellisonleao/gruvbox.nvim'
+	use ('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+	use { 'nvim-telescope/telescope.nvim', tag = '0.1.8', requires = { {'nvim-lua/plenary.nvim'} } }
 end)
