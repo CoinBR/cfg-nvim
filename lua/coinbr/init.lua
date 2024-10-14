53245f5dfee8eefc7ba97a64ed5454d62163eda4
CoinBR <coinbr@gmail.com>
2024-10-14 20:51:08 -0300

    add virtual col to help limitting git commit msg

--- lua/coinbr/init.lua
+++ lua/coinbr/init.lua
@@ -1,3 +1,4 @@
 require("coinbr/colorscheme")
+require("coinbr/virt-column")
 require("coinbr/packer")
 require("coinbr/remap")
--- lua/coinbr/packer.lua
+++ lua/coinbr/packer.lua
@@ -6,6 +6,7 @@ vim.cmd [[packadd packer.nvim]]
 return require('packer').startup(function(use)
 	use 'wbthomason/packer.nvim' -- Packer can manage itself
 	use 'ellisonleao/gruvbox.nvim'
+	use 'lukas-reineke/virt-column.nvim'
 	use ('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
 	use { 'nvim-telescope/telescope.nvim', tag = '0.1.8', requires = { {'nvim-lua/plenary.nvim'} } }
 end)
