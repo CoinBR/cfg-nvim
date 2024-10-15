bfc7dade39bc4d950580e26e2dc52fbb161a5594
CoinBR <coinbr@gmail.com>
2024-10-15 19:01:16 -0300

    add undotree plugin

--- lua/coinbr/lazy.lua
+++ lua/coinbr/lazy.lua
@@ -21,6 +21,7 @@ require("lazy").setup({
     -- import your plugins
     'ellisonleao/gruvbox.nvim',
     'lukas-reineke/virt-column.nvim',
+    'mbbill/undotree',
     {'nvim-treesitter/nvim-treesitter', build = ':TSUpdate' },
     {'nvim-telescope/telescope.nvim', tag = '0.1.8', dependencies = { 'nvim-lua/plenary.nvim' },
     {'ThePrimeagen/harpoon', dependencies = { 'nvim-lua/plenary.nvim' } },
