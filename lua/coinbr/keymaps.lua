292ccf3e05a8456f79050e4f70ee33b06d3d5a05
CoinBR <coinbr@gmail.com>
2025-02-06 13:32:26 -0300

    make S-Tab binding more intuitive

--- lua/coinbr/keymaps.lua
+++ lua/coinbr/keymaps.lua
@@ -1,3 +1,9 @@
 -- there are more keymaps on the plugins folder, inside the plugins config files
 
 vim.g.mapleader = " "
+
+vim.keymap.set("n", "<C-h>", function() ui.nav_file(1) end, {desc = "1st Harpoon"})
+
+vim.keymap.set('i', '<S-Tab>', '<C-d>', {noremap = true, desc = "↹ Backward tab (unindent)"})
+vim.keymap.set('n', '<leader>shift↹', '<S-Tab>', {noremap = true, desc = "[insert mode] ↹ Backward tab (unindent)"})
+
