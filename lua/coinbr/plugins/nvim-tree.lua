e4229b5efe78daf7a0cba169dca600ebcbb7800d
CoinBR <coinbr@gmail.com>
2025-02-06 13:10:41 -0300

    nvim-tree: improve file explorer toggle hotkeys

--- lua/coinbr/plugins/nvim-tree.lua
+++ lua/coinbr/plugins/nvim-tree.lua
@@ -79,10 +79,46 @@ return {
     })
 
 
-    vim.keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", {desc = "Toggle file explorer"})
+    vim.keymap.set("n", "<leader>eh", "<cmd>NvimTreeToggle<CR>", {desc = "Toggle file explorer"})
     vim.keymap.set("n", "<leader>ef", "<cmd>NvimTreeFindFileToggle<CR>", {desc = "Focus current file on file explorer"})
     vim.keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", {desc = "Collapse all folders on file explorer"})
     vim.keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", {desc = "Refresh file explorer"})
+
+    vim.keymap.set('n', '<leader>ee', function()
+      local nvim_tree_wins = {}
+      local current_win = vim.api.nvim_get_current_win()
+      local all_wins = vim.api.nvim_list_wins()
+      local current_buf = vim.api.nvim_get_current_buf()
+
+      for _, w in pairs(all_wins) do
+        local buf = vim.api.nvim_win_get_buf(w)
+        local buf_name = vim.api.nvim_buf_get_name(buf)
+        if buf_name:match("NvimTree_") then
+          table.insert(nvim_tree_wins, w)
+        end
+      end
+
+      if #nvim_tree_wins > 0 then
+        if vim.bo[current_buf].filetype == "NvimTree" then
+          -- We're in NvimTree, so find a non-NvimTree window to focus
+          for _, w in pairs(all_wins) do
+            if w ~= current_win then
+              local buf = vim.api.nvim_win_get_buf(w)
+              if vim.bo[buf].filetype ~= "NvimTree" then
+                vim.api.nvim_set_current_win(w)
+                return
+              end
+            end
+          end
+        else
+          -- We're not in NvimTree, so focus the NvimTree window
+          vim.api.nvim_set_current_win(nvim_tree_wins[1])
+        end
+      else
+        vim.cmd("NvimTreeToggle")
+      end
+    end, {desc = "Switch focus between file and explorer"})
+
 
   end
 }
