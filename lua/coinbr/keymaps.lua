49bdc85155adebadeaabd370378d9589bf24e7a6
CoinBR <coinbr@gmail.com>
2025-02-06 13:53:57 -0300

    bind esc to normal mode & clear search highlights

--- lua/coinbr/keymaps.lua
+++ lua/coinbr/keymaps.lua
@@ -2,8 +2,15 @@
 
 vim.g.mapleader = " "
 
+local function enter_normal_mode_and_clear_search_highlights()
+    vim.cmd('nohlsearch')
+    vim.cmd('stopinsert')
+end
+
 vim.keymap.set("n", "<C-h>", function() ui.nav_file(1) end, {desc = "1st Harpoon"})
 
 vim.keymap.set('i', '<S-Tab>', '<C-d>', {noremap = true,          desc = "[insert mode] ↹ Backward tab (unindent)"})
 vim.keymap.set('n', '<leader>shift↹', '<S-Tab>', {noremap = true, desc = "[insert mode] ↹ Backward tab (unindent)"})
 
+vim.keymap.set({'i', 'n', 'v'}, '<Esc>', enter_normal_mode_and_clear_search_highlights, {noremap = true, silent = true, desc = "Enter normal mode and clear search highlights"})
+vim.keymap.set({'i', 'n', 'v'}, '<C-[>', enter_normal_mode_and_clear_search_highlights, {noremap = true, silent = true, desc = "Enter normal mode and clear search highlights"})
